9819cb89e98f03620bbc38b244facf4c965071e2
fatfirebird <fatfirebird@gmail.com>
2023-09-06 16:36:21 +0400

    add creating user transaction

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,37 +1,54 @@
 import { Inject, Injectable, forwardRef } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/infra/database';
-import { Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private userRepository: Repository<User>,
     @Inject(forwardRef(() => AuthService))
     private authService: AuthService,
+    private dataSource: DataSource,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const hash = await bcrypt.hash(createUserDto.password, 10);
+    const queryRunner = this.dataSource.createQueryRunner();
 
-    const newUser = this.userRepository.create({
-      ...createUserDto,
-      password: hash,
-    });
+    let data = { accessToken: null, refreshToken: null, user: null };
 
-    const user = await this.userRepository.save(newUser);
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    const { accessToken, refreshToken } = await this.authService.createTokens(
-      user,
-    );
+    try {
+      const hash = await bcrypt.hash(createUserDto.password, 10);
 
-    return {
-      user: user,
-      accessToken,
-      refreshToken,
-    };
+      const newUser = queryRunner.manager.create(User, {
+        ...createUserDto,
+        password: hash,
+      });
+
+      const user = await queryRunner.manager.save(newUser);
+
+      const { accessToken, refreshToken } = await this.authService.createTokens(
+        user,
+      );
+
+      await queryRunner.commitTransaction();
+
+      data = {
+        accessToken,
+        refreshToken,
+        user,
+      };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw new Error('Creating user error');
+    } finally {
+      await queryRunner.release();
+    }
+
+    return data;
   }
 }
